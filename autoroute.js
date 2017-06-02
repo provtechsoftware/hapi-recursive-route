5b6d046a3aa3a82b46eb502ae97765c739b4864b
Felipe Provenzano <felipeprov@gmail.com>
2017-06-02 11:39:21 -0300

    Starting repository
    Signed-off-by: Felipe Provenzano <felipeprov@gmail.com>

---
+++ autoroute.js
@@ -0,0 +1,34 @@
+'use strict';
+const debug = require('debug')('autoroute');
+const Path = require('path');
+const walk = require('fs-walk');
+
+const testDefault = /\.(js)$/;
+
+exports.register = (server, baseOptions, next) => {
+	debug('AutoRoute Registered', baseOptions);
+
+	return walk.walk(baseOptions.dir, (dir, filename, stat, next) =>{
+		debug('filename:', filename, ', ', dir,', ', stat.isFile());
+
+		if(stat.isFile()){
+			const modulePath = Path.join(dir, filename);
+			debug('Registering:', modulePath);
+            const routeFile = require(modulePath);
+            routeFile.routes(server);
+		}
+		next();
+	}, (err)=>{
+		if(err){
+			return next(err);
+		}
+
+		debug('Ended');
+		return next();
+	});
+};
+
+exports.register.attributes = {
+	name: 'AutoRoutes',
+    version: '0.0.1'
+};
